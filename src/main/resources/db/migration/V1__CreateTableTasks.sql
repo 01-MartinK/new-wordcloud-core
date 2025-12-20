92ac3eefa197628326bce8eab7ca6b520c415236
Martin K <nitram1509@gmail.com>
2025-12-20 17:44:06 +0200

    Create flywheel migrations

---
+++ src/main/resources/db/migration/V1__CreateTableTasks.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS TAGCLOUDS
+(
+    id VARCHAR(32) NOT NULL PRIMARY KEY,
+    status INT,
+    word_list TEXT
+);
---
+++ src/main/resources/db/migration/V2__TestData.sql
@@ -0,0 +1,2 @@
+insert into tagclouds(id, status, word_list) values ('KCOT2gg_O8L-jm8s4Y8-G', 0, null);
+insert into tagclouds(id, status, word_list) values ('joLyAKL3C6BunjvXMl4LY', 0,  null);
