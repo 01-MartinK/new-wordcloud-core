9e9543f11532e61544c64d457f9e10ceb4bc6a82
Martin K <nitram1509@gmail.com>
2025-12-21 17:49:37 +0200

    Remove redundant semicolons

--- src/main/kotlin/com/mk/wordcloud/service/StorageService.kt
+++ src/main/kotlin/com/mk/wordcloud/service/StorageService.kt
@@ -3,7 +3,7 @@ package com.mk.wordcloud.service
 import org.springframework.web.multipart.MultipartFile
 
 interface StorageService {
-    fun init();
-    fun store(file: MultipartFile, id: String): String;
-    fun deleteAll();
+    fun init()
+    fun store(file: MultipartFile, id: String): String
+    fun deleteAll()
 }
