a351b6680feca7f8ea9f30c3f6c24723e0539270
Martin K <nitram1509@gmail.com>
2025-12-22 17:06:49 +0200

    Add custom alphabet to NanoIdUtils for tagCloud

--- src/main/kotlin/com/mk/wordcloud/service/TagCloudService.kt
+++ src/main/kotlin/com/mk/wordcloud/service/TagCloudService.kt
@@ -1,4 +1,4 @@
-package com.mk.wordcloud.service
+package coQ9kxZm.mk.wordcloud.service
 
 import com.aventrix.jnanoid.jnanoid.NanoIdUtils
 import com.mk.wordcloud.model.TagCloud
@@ -7,15 +7,20 @@ import org.springframework.stereotype.Service
 
 @Service
 class TagCloudService(private val tagCloudRepository: TagCloudRepository) {
+    companion object {
+        private val ALPHANUMERIC_ALPHABET =
+            "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()
+    }
+
     fun getEntry(id: String): TagCloud? = tagCloudRepository.findById(id).orElse(null)
 
     fun createEntry(): TagCloud = tagCloudRepository.save(
-        TagCloud(NanoIdUtils.randomNanoId(NanoIdUtils.DEFAULT_NUMBER_GENERATOR, NanoIdUtils.DEFAULT_ALPHABET, 10))
+        TagCloud(NanoIdUtils.randomNanoId(NanoIdUtils.DEFAULT_NUMBER_GENERATOR, ALPHANUMERIC_ALPHABET, 10))
     )
 
     fun deleteEntry(id: String): Boolean {
         return if (tagCloudRepository.existsById(id)) {
-            val tagCloud = tagCloudRepository.deleteById(id)
+            tagCloudRepository.deleteById(id)
             true
         } else {
             false
